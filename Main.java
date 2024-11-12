52af0e36234dfd848dae05f3ede7c73007277382
2382019 <2382019@unai.edu>
2024-11-12 10:24:18 +0700

    handson8

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import config.Database;
 import repositories.ToDoListRepository;
 import repositories.ToDoListRepositoryImpl;
+import repositories.TodoListRepositoryDbImpl;
 import services.ToDoListService;
 import services.ToDoListServiceImpl;
 import views.ToDoListTerminalView;
@@ -7,7 +9,11 @@ import views.ToDoListView;
 
 public class Main {
     public static void main(String[] args) {
-        ToDoListRepository toDoListRepository= new ToDoListRepositoryImpl();
+        Database database = new Database("databaseku", "root", "", "localhost", "3306");
+        database.setup();
+
+
+        ToDoListRepository toDoListRepository= new TodoListRepositoryDbImpl(database);
         ToDoListService toDoListService= new ToDoListServiceImpl(toDoListRepository);
         ToDoListView toDoListView = new ToDoListTerminalView(toDoListService);
         toDoListView.run();
